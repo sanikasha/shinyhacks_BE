32726ec41a69f84f096596149d8cc3bbf5755439
sanikasha <111531356+sanikasha@users.noreply.github.com>
2023-04-30 19:24:07 -0700

    Update api.py

--- hacks_api/api/api.py
+++ hacks_api/api/api.py
@@ -1,18 +1,100 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource, reqparse
 from .. import db
-from ..model.model import # class from model
+from ..model.model import Student
 
-example_bp = Blueprint("leaderboards", __name__)
-example_api = Api(example_bp)
+student_bp = Blueprint("students", __name__)
+student_api = Api(student_bp)
+
+def get_name_list():
+    names_list = [[name._name] for name in Student.query.all()]
+    return names_list
+
+def find_by_name(name):
+    names = Student.query.filter_by(_name=name).all()
+    return names[0]
 
 
-class ExampleAPI(Resource):
-# make sure to have all of CRUD made here
-# make a second api for images as well, in a different file
+class StudentAPI(Resource):
+    def get(self):
+        name = request.get_json().get("name")
+        print(name, "name")
+        name = find_by_name(name)
+        if name:
+            return name.to_dict()
+        return {"message": name}, 404
 
-class ExampleListAPI(Resource):
-# edit the links below in order to 
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", required=True, type=str)
+        parser.add_argument("GPA", required=True, type=int)
+        parser.add_argument("grade", required=True, type=int)
+        args = parser.parse_args()
 
-example_api.add_resource(ExampleAPI, "/data-points")
-example_api.add_resource(ExampleListAPI, "/database")
+        student = Student(args["name"], args["GPA"],
+                                  args["grade"])
+        try:
+            db.session.add(student)
+            db.session.commit()
+            return student.to_dict(), 201
+        except Exception as e:
+            db.session.rollback()
+            return {"message": f"server error: {e}"}, 500
+
+    def put(self):
+        name = request.get_json().get("name")
+        print(name, "name")
+
+        try:
+            name = find_by_name(name)
+            if name:
+                name.GPA = int(request.get_json().get("GPA"))
+                name.grade = int(request.get_json().get("grade"))
+                db.session.commit()
+                return name.to_dict(), 201
+            else:
+                return {"message": "student not found"}, 404
+        except Exception as e:
+            db.session.rollback()
+            return {"message": f"server error: {e}"}, 500
+
+    def delete(self):
+        name = request.get_json().get("name")
+        print(name, "name")
+
+        try:
+            name = find_by_name(name)
+            if name:
+                db.session.delete(name)
+                db.session.commit()
+                return name.to_dict()
+            else:
+                return {"message": "studnet not found"}, 404
+        except Exception as e:
+            db.session.rollback()
+            return {"message": f"server error: {e}"}, 500
+
+
+class StudentListAPI(Resource):
+    def get(self):
+        try:
+            students = db.session.query(Student).all()
+            return [student.to_dict() for student in students]
+        except Exception as e:
+            db.session.rollback()
+            return {"message": f"server error: {e}"}, 500
+
+    def delete(self):
+        try:
+            db.session.query(Student).delete()
+            db.session.commit()
+            return []
+        except Exception as e:
+            db.session.rollback()
+            return {"message": f"server error: {e}"}, 500
+        
+
+
+student_api.add_resource(StudentAPI, "/student")
+student_api.add_resource(StudentListAPI, "/studentList")
+
